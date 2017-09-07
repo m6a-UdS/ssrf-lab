84961a55a9a01cdf89fa20c4c3f8951187063dc0
Max <max@localhost.localdomain>
2017-09-06 22:45:38 -0400

    Work in progress

--- advanced2/secretserver/app.py
+++ advanced2/secretserver/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'My internal (ie: secured) Flask service!\nOnly accessible from 10.0.0.0/8!'
+    return 'This is secretserver1!\nOnly accessible from 10.0.0.0/8!'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
